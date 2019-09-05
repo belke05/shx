df393bf9245711c720f7c8b43ad7f234689d7f9c
sara sebti <sarasebti@MacBook-Pro-de-sara.local>
2019-09-05 18:44:17 +0200

    -do not consider

--- models/Sneaker.js
+++ models/Sneaker.js
@@ -9,12 +9,11 @@ const SneakerSchema = new Schema({
   price: { type: String },
   category: { type: Array, enum: ["women", "men", "children"] },
   id_tags: { type: Schema.Types.ObjectId },
-  ref: "Tag"
+  ref: ["Tag"]
   // how to link document from different collections ?
   // tells Mongoose this ID connects to the "Category" model
 });
 
-productSchema.index({ ref: 1 }, { unique: true }); // ensure unique ref
 const Sneaker = mongoose.model("Sneaker", SneakerSchema);
 
 module.exports = Sneaker;
