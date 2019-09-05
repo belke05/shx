90caa62d2ecceef8bc759b6aded66e1cbbac871f
belke05 <you@example.com>
2019-09-05 18:24:49 +0200

    change on sneaker model and product add route

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: String,
+  firstName: String,
   lastName: String,
   email: String,
   password: String
@@ -11,4 +11,3 @@ const userSchema = new Schema({
 const userModel = mongoose.model("User", userSchema);
 
 module.exports = userModel;
-
