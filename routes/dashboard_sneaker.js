07c5aafeabb62177042421a6d6556c58b5379ee7
belke05 <you@example.com>
2019-09-06 09:02:07 +0200

    added tag rendering in product add page

--- routes/dashboard_sneaker.js
+++ routes/dashboard_sneaker.js
@@ -2,19 +2,70 @@ const express = require("express");
 const router = new express.Router();
 const Sneaker = require("../models/Sneaker.js");
 const Tags = require("../models/Tag.js");
+const uploadMiddleware = require("../config/cloudinary.js");
 
 router.get("/prod-add", (req, res, next) => {
   Tags.find()
     .then(tags => {
+      console.log("the tags are", tags);
       res.render("products_add", { tags });
     })
     .catch(dbErr => {
-      console.log("error rendering the tags", dbErr);
+      console.log("error finding the tags", dbErr);
     });
 });
 // ENDNENENENENENENEENE HERE
-router.post("/prod-add", (req, res, next) => {
-  const { sneaker } = body.req;
+router.post(
+  "/prod-add",
+  uploadMiddleware.single("sneaker_img"),
+  (req, res, next) => {
+    const {
+      name,
+      ref,
+      sizes,
+      description,
+      price,
+      category,
+      id_tags
+    } = req.body;
+    const newSneaker = {
+      name,
+      ref,
+      sizes,
+      description,
+      price,
+      category,
+      id_tags
+    };
+    if (req.file) {
+      newSneaker.imgName = req.file.originalname;
+      newSneaker.imgPath = req.file.secure_url;
+    }
+    Sneaker.create(newSneaker)
+      .then(createdSneaker => {
+        console.log(createdSneaker, "was created");
+        res.redirect("/prod-add");
+      })
+      .catch(dbErr => {
+        console.log("error during creation of new sneaker", dbErr);
+      });
+  }
+);
+
+// route to add tags redirecting back to prod-add so that the new tag can be selected
+// for the shoe
+router.post("/tag-add", (req, res, next) => {
+  const { label } = req.body;
+  console.log(req.body);
+  const newTag = { label };
+  Tags.create(newTag)
+    .then(createdTag => {
+      console.log(createdTag, "was created as a tag");
+      res.redirect("/prod-add");
+    })
+    .catch(dbErr => {
+      console.log("error creating a new tag", dbErr);
+    });
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const userModel = require ("../models/User");
+const userModel = require("../models/User");
 const sneakers = require("../models/Sneaker");
+const tags = require("../models/Tag");
 
 router.get("/", (req, res) => {
   res.render("index");
@@ -9,23 +10,32 @@ router.get("/", (req, res) => {
 
 router.get("/sneakers/:cat", (req, res) => {
   const cat = req.params.cat;
-  sneakers.find({ category: cat })
-  .then(category =>{
-    res.render("products",{ category });
-  })
-  .catch(err =>{console.log(err);
-  });
-  
+  tags
+    .find()
+    .then(tags => {
+      sneakers
+        .find()
+        .then(category => {
+          console.log("the shoes in the category are", category);
+          res.render("products", { category, tags });
+        })
+        .catch(console.log("error looking up sneakers"));
+    })
+    .catch(err => {
+      console.log("error looking up tags", err);
+    });
 });
 
-
-router.get("/sneakers",(req,res)=>{
-  sneakers.find().then(
-    sneakers => res.render("products",{sneakers}),
-    console.log("sneakers")
-  ).catch( err =>{
-    console/log(err);
-  });
+router.get("/sneakers", (req, res) => {
+  sneakers
+    .find()
+    .then(
+      sneakers => res.render("products", { sneakers, tags }),
+      console.log("sneakers")
+    )
+    .catch(err => {
+      console / log(err);
+    });
 });
 
 router.get("/one-product/:id", (req, res) => {
@@ -40,14 +50,13 @@ router.get("/signin", (req, res) => {
   res.render("signin");
 });
 
-router.get("/home", (req,res)=>{
+router.get("/home", (req, res) => {
   res.redirect("/");
 });
 
-router.get("/logout", (req,res)=>{
+router.get("/logout", (req, res) => {
   req.session.destroy();
   res.redirect("/");
-})
+});
 
 module.exports = router;
-
