18ecb439dc8f7b3310fed98ec12f1336843ff897
belke05 <you@example.com>
2019-09-06 17:41:55 +0200

    filter function update

--- public/scripts/filter.js
+++ public/scripts/filter.js
@@ -1,6 +1,5 @@
-const inputTags = document.querySelectorAll("input");
+let inputTags = document.querySelectorAll("input");
 let filterTags = [];
-let displayed = [];
 
 function updateTags() {
   filterTags = [];
@@ -14,44 +13,34 @@ function updateTags() {
     .post(`/filter/tags`, { tags: filterTags })
     .then(servRes => {
       console.log("data response", servRes.data);
-      servRes.data.forEach(res => {
-        console.log(res._id);
-      });
-      let allowedSneakers = [];
-      servRes.data.forEach(obj => {
-        allowedSneakers.push(obj._id);
-      });
-      displaycheck(allowedSneakers);
+      addsneakers(servRes.data);
+      // displaycheck(allowedSneakers);
     })
     .catch(err => {
       console.log(err);
     });
-  // filterTags.forEach(filter => {
-  //   axios
-  //     .get(`/filter/${filter}`)
-  //     .then(servRes => {
-  //       displayed.push(servRes.data[0]._id);
-  //       displaycheck(displayed, items);
-  //       console.log(displayed, "display shoes");
-  //     })
-  //     .catch(dbErr => {});
-  // });
 }
 
-function displaycheck(allowedSneakers) {
-  let items = document.getElementsByClassName("product-item-wrapper");
-  for (let i = 0; i < items.length; i++) {
-    console.log(items[i].id, "yaya");
-    console.log("item id", items[i].id);
-    console.log(allowedSneakers, "allowed");
-    if (allowedSneakers.includes(items[i].id)) {
-      items[i].hidden = false;
-    } else {
-      items[i].hidden = true;
-    }
-  }
+function addsneakers(snks) {
+  let products = document.getElementById("products_grid");
+  products.innerHTML = "";
+  snks.forEach(sneaker => {
+    let sneak = document.createElement("a");
+    sneak.href = `/one-product/${sneaker._id}`;
+    sneak.className = "product-item-wrapper";
+    sneak.innerHTML = `<div class="product-img">
+                   <img src="${sneaker.imgPath}" alt=${sneaker.name} : what a nice pair of kicks">
+             </div>
+             <p class="product-name">${sneaker.name}</p>
+              <p class="product-cat">${sneaker.category}</p>
+              <p class="product-price">${sneaker.price}</p>`;
+    products.appendChild(sneak);
+  });
 }
 
-inputTags.forEach(inputTags => {
-  inputTags.onchange = updateTags;
+inputTags.forEach(tag => {
+  tag.onclick = updateTags;
 });
+// `<a href="/one-product/${}" class="product-item-wrapper" id="5d725d10c368034b44f95855">
+//
+//       </a>`
